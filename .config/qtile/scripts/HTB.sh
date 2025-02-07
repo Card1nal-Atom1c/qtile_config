8c810a93880f54da1bee5b484cf68def84f77888
Card1nal-Atom1c <term1nus.atom1c@gmail.com>
2025-02-07 16:20:50 -0300

    Create HTB.sh

---
+++ .config/qtile/scripts/HTB.sh
@@ -0,0 +1,8 @@
+!# /bin/bash
+IFACE=$(ip address| grep tun0 | awk '{print $1}' | tr -d ':')
+
+if [ "$IFACE" = "tun0" ]; then
+   echo "VPN:$(ip address tun0 | grep "inet " | awk '{print $2}')${u-}"
+else
+   echo "VPN:Disconnected"
+fi
